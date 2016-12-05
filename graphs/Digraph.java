382f0d7ce90a45d44dc7c71d0354ae6655a6bc68
Arkainoh <arkainoh@gmail.com>
2016-12-06 01:17:00 +0900

    Fix misconceptions. toString() prints edges as arrows

--- graphs/Digraph.java
+++ graphs/Digraph.java
@@ -5,11 +5,16 @@ public class Digraph extends Graph{
 	public Digraph(int V) {
 		super(V);
 	}
-
-	public void addEdge(int v, int w) {
-		adj[v].add(w);
-		adj[w].add(v);
-		E++;
+	
+	public String toString() {
+		sb = new StringBuffer();
+		sb.append("V: " + V + "\nE: " + E + "\n");
+		for(int v = 0; v < V; v++) {
+			for(int w : adj[v]) {
+				sb.append(v + "->" + w + "\n");
+			}
+		}
+		return sb.toString();
 	}
 	
 }
