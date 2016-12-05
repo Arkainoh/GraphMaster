0ebb4b569091f3e35915f3f6471717f1dfb7a425
Arkainoh <arkainoh@gmail.com>
2016-12-06 01:58:59 +0900

    Add a data structure, WeightedEdge

---
+++ graphs/WeightedEdge.java
@@ -0,0 +1,19 @@
+package graphs;
+
+public class WeightedEdge<T> {
+	public int from;
+	public int to;
+	public T weight;
+	
+	public WeightedEdge() {}
+	
+	public WeightedEdge(int v, int w, T weight) {
+		this.from = v;
+		this.to = w;
+		this.weight = weight;
+	}
+	
+	public String toString() {
+		return "" + from + "-" + to + ":" + weight;
+	}
+}
